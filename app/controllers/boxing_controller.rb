a57eb71af81a5a213394be4ec40ed0c3de90a6f6
Stephen Kelly <steviek@Stephens-MacBook-Pro.local>
2021-04-05 09:11:36 +1000

    third commit i have successully created my create route from postman to server

--- app/controllers/boxing_controller.rb
+++ app/controllers/boxing_controller.rb
@@ -1,6 +1,29 @@
 class BoxingController < ApplicationController
-    def index 
-        render json: {greeeting: 'Hello Boxing'
-    }
-end
+  def index 
+        render json: {type: 'index'}
+  end
+
+  def create 
+    boxing = Boxing.create(name: params[:name], weight_id: params[:weight_id], country: params[:country])
+    boxing_valid = boxing.valid?
+    p boxing 
+    p boxing_valid
+    if boxing_valid
+    render json: {message: 'Successfully created entry!'}, status: 200
+    else 
+    render json: {message: 'Unable to create entry!'}, status: 400
+    end
+  end 
+
+  def show 
+    render json: {type: 'show'}
+  end
+
+  def update 
+    render json: {type: 'update'}
+  end
+
+  def destroy  
+    render json: {type: 'destroy'}
+  end
 end
