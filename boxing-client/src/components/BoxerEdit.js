e0d02159e6cce3efde09859baa2d6091515fbd4a
Stephen Kelly <steviek@Stephens-MacBook-Pro.local>
2021-04-06 22:20:20 +1000

    rendered back-end info by params on to the ui form on BoxerEdit

--- boxing-client/src/components/BoxerEdit.js
+++ boxing-client/src/components/BoxerEdit.js
@@ -1,10 +1,36 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router"
 
-export const BoxerEdit = () => {
+export const BoxerEdit = (props) => {
+    const [boxing, setBoxing] = useState({
+        id: null,
+        name: "",
+        weight_id: null,
+        country: ""
+    })
     const params = useParams();
     console.log('params:', params);
+    useEffect(() => {
+        fetch(`/boxing/${params.id}`)
+          .then(response => response.json())
+          .then(data => setBoxing(data))
+      }, [params.id])
 
     return <div>
         <h1>Entry Edit</h1>
+        <form>
+            <label>
+                Name:
+                <input value={boxing.name} />
+            </label>
+            <label>
+                Weight_id:
+                <input value={boxing.weight_id} />
+            </label>
+            <label>
+                Country:
+                <input value={boxing.country} />
+            </label>
+        </form>
     </div>
 }
